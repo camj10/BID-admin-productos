6b4dfc8e6d424769d2b1a37ebfdcddf37f76b1c4
camj10 <camilajisel06@gmail.com>
2023-02-17 18:46:11 -0300

    prueba MERN

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+
+require('./routes/person.routes')(app); 
+
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
-});
+})
