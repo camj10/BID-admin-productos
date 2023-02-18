fa62a2b420a8b856ce371051514c1b55a7e32ea4
camj10 <camilajisel06@gmail.com>
2023-02-17 22:12:48 -0300

    backend

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+require('dotenv').config()
+
 const express = require('express');
+const cors = require('cors');
+const puerto = process.env.PUERTO;
 const app = express();
-
-require('./routes/person.routes')(app); 
-
+require('./config/mongoose.config'); // This is new
+app.use(cors());
+app.use(express.json()); // This is new
+app.use(express.urlencoded({ extended: true })); // This is new
+require('./routes/person.routes')(app);
 app.listen(8000, () => {
     console.log("Listening at Port 8000")
 })
